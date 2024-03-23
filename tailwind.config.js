b1709d35ad3c7747475896993b58b23979b9b60b
Stobles <ilovespnjensen@inbox.ru>
2024-03-23 20:18:38 +0300

    chore: delete unnecessary files and add tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,60 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      roboto: ["Roboto", "sans-serif"],
+    },
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        xs: "420px",
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1536px",
+      },
+    },
+    extend: {
+      colors: {
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        "background-input": "hsl(var(--background-input))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          second: "hsl(var(--primary-second))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+        },
+      },
+      borderColor: {
+        input: "var(--border-input)",
+        destructive: "hsl(var(--destructive))",
+        foreground: "var(--border-foreground)",
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      boxShadow: {
+        popover: "var(--popover-shadow)",
+        card: "var(--card-shadow)",
+      },
+    },
   },
   plugins: [],
-}
-
+};
