6417abdd162174393a16ec25867006f88cc1134e
Stobles <ilovespnjensen@inbox.ru>
2024-03-25 10:26:32 +0300

    chore: move components

--- src/views/Home/HomePage.tsx
+++ src/views/Home/HomePage.tsx
@@ -1,10 +1,7 @@
-import { ProductsList } from "./components/ProductsList";
+import { ProductsList } from "@/components/ProductsList";
+import { useGetProductsQuery } from "@/features/products/api/productsApi";
 
 export const HomePage = () => {
-  return (
-    <div className="w-full h-full mt-2">
-      <h2 className="text-lg font-semibold text-secondary">Товары - 12</h2>
-      <ProductsList />
-    </div>
-  );
+  const { data: products, isLoading } = useGetProductsQuery({});
+  return <ProductsList products={products} isLoading={isLoading} />;
 };
